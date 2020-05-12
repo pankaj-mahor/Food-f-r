168bfa63ce01dd98784345a14a3d317e08602a8d
pankaj-mahor <58332081+pankaj-mahor@users.noreply.github.com>
2020-05-12 15:34:03 +0530

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,8 @@
+L.mapquest.key = 'DCh9dfWK2FC2ARYqkqkGajKSjlAGc0HM';
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
